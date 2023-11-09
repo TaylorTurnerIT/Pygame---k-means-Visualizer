2f619fd9f6e6410b7fcc9414a6714d2cfa069e66
Taylor T <taylorturnerit@gmail.com>
2023-11-09 17:32:14 -0600

    Optimized cluster generation

--- kmeans.py
+++ kmeans.py
@@ -34,45 +34,51 @@ class Point:
         pygame.draw.circle(canvas, self.c, self.origin, self.radius)
 
 
-# GAME OBJECTS
-dataCount = 20
-dataList = []
-clusterCount = 3
-clusterList = []
+class DataCreator:
+    def __init__(self):
+        # GAME OBJECTS
+        self.dataCount = 250 # number of data points to generate
+        self.dataList = [] # array to store all data points
+        self.clusterCount = 3 # number of clusters to generate, also used to determine number of clusters for a k-means alg.
+        self.clusterList = [] # Stores the cluster 
+        self.genRadius = 50 # Radius of the generation circle
+        self.pointDrawRadius = 3 # Size of each data point (visual only)
+        self.pointColor = "black"
+        self.generateClusters() # When the object is created, generate the clusters, then the data
+        self.generateData()
 
-# QUADRANT CREATION
-# This system will define a cluster's positioning by creating a circular area in which data can appear. The more clusters, the less there are. Also ensures two clusters do not overlap.
-for i in range(clusterCount):
-    clusterOrigin = (random.randint(0, screen.screenWidth),random.randint(0, screen.screenHeight)) # Computes a random point to serve as the cluster's center. All points will be within a radius of this point
-    radius = 20
-    clusterList.append(clusterOrigin)
+    # CLUSTER CREATION
+        # This system will define a cluster's positioning by creating a circular area in which data can appear. The more clusters, the less data is in each.
 
-    
+    # Generates the clusters and adds them to the list
+    def generateClusters(self):
+        for i in range(self.clusterCount):
+            self.clusterOrigin = pygame.Vector2(int(random.randint(self.genRadius, screen.screenWidth-self.genRadius)),int(random.randint(self.genRadius, screen.screenHeight-self.genRadius))) # Computes a random point to serve as the cluster's center. All points will be within a radius of this point
+            self.clusterList.append(self.clusterOrigin)
+            print("Cluster origin is at ")
+            print(self.clusterOrigin)
+
+    # Generates the data within the clusters, the clusters MUST be generated before this can work.
+    def generateData(self):
+        for n in range(self.dataCount):
+            cluster = random.randint(0, self.clusterCount-1) # Between 1 and cluster count, but zero indexed
+            origin = self.clusterList[cluster] # Gets current cluster
+            randCoordinate = pygame.Vector2(0,0) # Defaults to 0,0
+            
+            angle = random.uniform(0,1) * 2 * np.pi # Gets a random angle direction in a circular area
+            radius = self.genRadius * np.sqrt(random.uniform(0,1)) # Gets a random distance from the origin to generates
+            # Generates cartesian coordinates from angle and radius
+            randx = radius * np.cos(angle)+self.clusterOrigin.x
+            randy = radius * np.sin(angle)+self.clusterOrigin.y
+            randCoordinate = pygame.Vector2(randx, randy)
+            # This line appends the coordinate, radius, color, AND assigns each point to be neighbors with all others
+            newPoint = Point(randCoordinate, self.pointDrawRadius, self.pointColor, self.dataList)
+            screen.addDraw(newPoint, 1)
+            self.dataList.append(newPoint)
 
 
-# clusterList.append((0, int(screen.screenHeight/clusterCount)))
-# clusterList.append((int(screen.screenHeight/clusterCount), screen.screenHeight))
 
-for n in range(dataCount):
-    # Choose a random cluster to plot points
-    cluster = random.randint(0, clusterCount-1)
-    randx = random.randint()
-    randy = int(random.randint(clusterList[cluster][0], clusterList[cluster][1]))
-    randCoordinate = pygame.Vector2(randx, randy)
-    # Color black
-    color = "black"
-
-    # Color by cluster at creation
-    # if(cluster == 0):
-    #     color = "dark blue"
-    # else:
-    #     color = "dark red"
-    rad = 4
-    # This line appends the coordinate, radius, color, AND assigns each point to be neighbors with all others
-    dataList.append(Point(randCoordinate, rad, color, dataList))
-
-for point in dataList:
-    screen.addDraw(point, 1)
+Data = DataCreator()
 
 # GAME LOOP
 while not exit: 
